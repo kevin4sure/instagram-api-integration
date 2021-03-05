0dec67ea036be2194cfc69f8b68bac10bdecc3d4
kevin4sure <kamleshguptaslg@gmail.com>
2021-03-05 10:02:14 +0530

    added inital auth route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,9 +10,15 @@ def index():
     return jsonify(msg="hello world!")
 
 
+@app.route("/auth", methods=["GET", "POST"])
+def authenticate():
+    reqData =  json.loads(request.data)
+    return jsonify(reqData)  
+
+
 port = int(os.getenv("PORT", 0))
 if __name__ == '__main__':
     if port != 0:
         app.run(host='0.0.0.0', port=port)
     else:
-        app.run(debug=True)    
+        app.run(debug=True, port=8080)    
